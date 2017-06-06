eb4b5a6f18b5fee65ef839b2309712c2eb203532
surenkund <skkund92@gmail.com>
2017-06-06 12:06:50 +0530

    another line of code

---
+++ su.java
@@ -0,0 +1,15 @@
+class Su{
+	public int sum(int x,int y){
+		return x+y;
+	}
+	
+	public static void main(String args[]){
+		Su ob=new Su();
+		ob.sum(10,20);
+		System.out.print(x+y);
+		System.out.print("new code");
+		System.out.print("new code2");
+		System.out.print("new code3");
+	}
+	
+}
